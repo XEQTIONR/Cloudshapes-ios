fd5aa0cd5b42e268ff21e1a73ceba0ba58865912
Ishtehar Hussain <ishteharhussain@Ishtehars-Mac-mini.local>
2015-03-18 03:09:27 -0400

    Trying to implement modal view controller from tab bar controller

---
+++ Cloudshapes/CSNewPostViewController.m
@@ -0,0 +1,56 @@
+//
+//  CSNewPostViewController.m
+//  Cloudshapes
+//
+//  Created by Ishtehar Hussain on 2015-03-17.
+//  Copyright (c) 2015 Cloudshapes. All rights reserved.
+//
+
+#import "CSNewPostViewController.h"
+
+@interface CSNewPostViewController ()
+
+@property (strong, nonatomic)NSString *postText;
+@property (strong, nonatomic)NSNumber *userId;
+@end
+
+@implementation CSNewPostViewController
+
+- (IBAction)post:(id)sender {
+    
+    NSUserDefaults *appDefaults = [NSUserDefaults standardUserDefaults];
+    NSString *stringUserId = [appDefaults objectForKey:@"userid"];
+    
+    self.userId = [NSNumber numberWithInt:[stringUserId intValue]];
+    self.postText = self.postTextView.text;
+    
+    [self resignFirstResponder];
+    
+    NSLog(@"Post text: %@ \n stringUserId : %@ \n userId : %d", self.postText, stringUserId, [self.userId intValue]);
+    
+    //code for script to insert new post
+   
+    
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
